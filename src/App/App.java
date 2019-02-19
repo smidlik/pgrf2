18fd04271bfc476bc72b0a6369cffa9b9de2a8ce
smidlik <smidajakub@email.cz>
2019-02-19 13:56:16 +0100

    SetPixel

---
+++ src/App/App.java
@@ -0,0 +1,14 @@
+package App;
+
+import ui.MainFrame;
+
+import javax.swing.*;
+
+public  class  App{
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(() -> {
+            MainFrame mainFrame = new MainFrame();
+            mainFrame.setVisible(true);
+        });
+    }
+}
