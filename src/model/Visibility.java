7e262ab72a2dc6ad2273fa7529a5d41a4f5ea798
smidlik <smidajakub@email.cz>
2019-02-19 13:55:50 +0100

    SetPixel

--- src/model/Visibility.java
+++ src/model/Visibility.java
@@ -4,15 +4,26 @@ public class Visibility {
 
     private ImgBuffer imgBuffer;
     private ZBuffer<Float> zBuffer;
+    private int widht;
+    private int height;
 
     public Visibility(int widht,int height) {
+        this.height=height;
+        this.widht=widht;
         this.imgBuffer = new ImgBuffer(widht,height);
         this.zBuffer = new ZBuffer<>(widht,height);
     }
 
-    public void put(float x,float y,float z, int color){
+    public void put(int x,int y,float z){
+        zBuffer.set(x,y,z);
+    }
 
-
-
+    public void init(){
+        for (int i = 0; i < imgBuffer.getHeight(); i++) {
+            for (int j = 0; j < imgBuffer.getHeight(); j++) {
+                imgBuffer.set(i,j,0);
+            }
+        }
+        zBuffer=new ZBuffer<>(height,widht);
     }
 }
--- src/ui/MainFrame.java
+++ src/ui/MainFrame.java
@@ -3,4 +3,23 @@ package ui;
 import javax.swing.*;
 
 public class MainFrame extends JFrame {
+
+    public MainFrame() {
+        init();
+    }
+
+
+    private void init() {
+        setTitle("PGRF2");
+        setSize(800, 600);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        setLocationRelativeTo(null);
+
+        initUI();
+
+    }
+    private void initUI() {
+
+    }
 }
